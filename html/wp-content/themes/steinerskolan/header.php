109baa3b11205be3619d3a82a14a59e3086f0e0d
Fvrom <from.ida@hotmail.com>
2021-10-01 13:42:03 +0200

    started on header

--- html/wp-content/themes/steinerskolan/header.php
+++ html/wp-content/themes/steinerskolan/header.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= get_theme_file_uri('assets/app.css') ?>">
+    <link rel="stylesheet" href="<?= get_theme_file_uri('assets/header.scss') ?>">
+    <link rel="stylesheet" href="<?= get_theme_file_uri('assets/style.scss') ?>">
     <?php wp_head(); ?>
 </head>
 
@@ -12,6 +13,9 @@
     <?php wp_body_open(); ?>
     <header>
         <nav role="navigation">
-            <?php wp_nav_menu(['theme_location' => 'navigation']); ?>
+            <?php wp_nav_menu( array( 'theme_location' => 'extra-menu',
+                        'menu_class' => 'header_menu'
+                         ) ); ?>
+            
         </nav>
     </header>
