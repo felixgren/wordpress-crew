200e56499a659dd827e19fe4dc95dc29b1ad8be5
felixgren <33127919+felixgren@users.noreply.github.com>
2021-10-01 14:16:59 +0200

    Commit Test

--- html/epictest.php
+++ html/epictest.php
@@ -1,6 +1,6 @@
 <?php
 
 echo 'Hellooooo!!!';
-echo 'wordpress is the best platform ever! so intuitive, so easy!'
-
+echo 'wordpress is the best platform ever! so intuitive, so easy!';
+echo 'this is a test commit to check that its all alright';
 ?>
