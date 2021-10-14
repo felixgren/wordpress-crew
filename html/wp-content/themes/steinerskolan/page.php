2f47f2dbb630168b0b1736341bc64d9f4d2474b5
felixgren <33127919+felixgren@users.noreply.github.com>
2021-10-14 03:10:32 +0200

    Resolve header sidescroll issue

--- html/wp-content/themes/steinerskolan/page.php
+++ html/wp-content/themes/steinerskolan/page.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
-<section>
+
+<main role="main">
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post() ?>
             <?= the_content() ?>
         <?php endwhile; ?>
     <?php endif; ?>
-</section>
+</main>
+
 <?php get_footer(); ?>
